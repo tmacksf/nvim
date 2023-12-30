d8aec559fc87f8d6e4c49645ddccce27c2022444
tmacksf <tmackcsb@gmail.com>
2023-12-30 12:34:12 +0000

    LSP
    Added more lsps to nvim-lsp and the associated changes in the others.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,15 @@
 return {
-  "williamboman/mason.nvim",
-  lazy = false,
-  cmd = "Mason",
-  config = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗",
-      },
-    },
-  },
+	"williamboman/mason.nvim",
+	lazy = false,
+	cmd = "Mason",
+	keys = { {"<leader>M", "<Cmd>Mason<CR>", desc = "Mason" }},
+	config = {
+		ui = {
+			icons = {
+				package_installed = "✓",
+				package_pending = "➜",
+				package_uninstalled = "✗",
+			},
+		},
+	},
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,23 +1,36 @@
-local opts = { noremap = true, silent = true, buffer = bufnr }
+local opts = { noremap = true, silent = true }
 
 local on_attach = function()
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true, silent = true, desc = "Go to declaration" })
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "Go to definition" })
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { noremap = true, desc = "Go to reference" })
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-	vim.keymap.set("n", "<space>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, opts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-	vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
-	vim.keymap.set("n", "<space>f", function()
+	vim.keymap.set(
+		"n",
+		"<space>cd",
+		vim.lsp.buf.type_definition,
+		{ noremap = true, silent = true, desc = "Go to type definition" }
+	)
+	vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "Rename" })
+	vim.keymap.set(
+		{ "n", "v" },
+		"<space>ca",
+		vim.lsp.buf.code_action,
+		{ noremap = true, silent = true, desc = "Code Action" }
+	)
+	vim.keymap.set("n", "<space>cf", function()
 		vim.lsp.buf.format({ async = true })
-	end, opts)
+	end, { noremap = true, silent = true, desc = "Format file" })
+
+	-- Workspace stuff
+	-- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+	-- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+	-- vim.keymap.set("n", "<space>wl", function()
+	-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	-- end, opts)
 end
 
 -- Lua
@@ -39,21 +52,46 @@ local lua_setup = {
 	},
 }
 
+-- Go
+local go_setup = {
+	on_attach = on_attach,
+	settings = {
+		Haskell = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/go")] = true,
+					[vim.fn.stdpath("config") .. "/go"] = true,
+				},
+			},
+		},
+	},
+}
+
 local config = function()
 	require("neoconf").setup({})
 
 	local lspconfig = require("lspconfig")
 
-	-- lua
+	-- Setting up lsps
 	lspconfig.lua_ls.setup(lua_setup)
+	lspconfig.hls.setup({ on_attach = on_attach })
+	lspconfig.clangd.setup({ on_attach = on_attach })
+	lspconfig.gopls.setup(go_setup)
 
+	-- Setting up formatters and linters
 	local luacheck = require("efmls-configs.linters.luacheck")
 	local stylua = require("efmls-configs.formatters.stylua")
+	local clangf = require("efmls-configs.formatters.clang_format")
 
 	-- efm config
 	lspconfig.efm.setup({
 		filetypes = {
 			"lua",
+			"c",
+			"cpp",
 		},
 		init_options = {
 			documentFormatting = true,
@@ -66,6 +104,8 @@ local config = function()
 		settings = {
 			languages = {
 				lua = { luacheck, stylua },
+				c = { clangf },
+				cpp = { clangf },
 			},
 		},
 	})
